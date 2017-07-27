41b6c0f5f0c155bf38490bfd1617e575064f9c6d
Jeffrey Eesley <jeffeesley@gmail.com>
2017-07-27 11:34:28 -0400

    Function, have not added styling

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+CREATE TABLE burgers(
+id int NOT NULL AUTO_INCREMENT,
+burger_name varchar(80) NOT NULL,
+devoured BOOL DEFAULT false,
+PRIMARY KEY (id)
+);
